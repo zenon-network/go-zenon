hypercore-one/hyperqube_z

Implement dynamic plasma ZIP
Implements the dynamic plasma ZIP and adds new momentum content ordering rules.

rpc/api/embedded/plasma.go
@@ -13,6 +13,7 @@ import (
 	"github.com/zenon-network/go-zenon/common"
 	"github.com/zenon-network/go-zenon/common/types"
 	"github.com/zenon-network/go-zenon/consensus"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/rpc/api"
 	"github.com/zenon-network/go-zenon/vm"
 	"github.com/zenon-network/go-zenon/vm/embedded/definition"
@@ -176,14 +177,26 @@ func (a *PlasmaApi) Get(address types.Address) (*PlasmaInfo, error) {
 		return nil, err
 	}
 
-	available, err := vm.AvailablePlasma(context.MomentumStore(), context)
-	if err != nil {
-		return nil, err
+	var availablePlasma uint64
+	var maxPlasma uint64
+
+	if context.IsDynamicPlasmaSporkEnforced() {
+		availablePlasma, err = vm.AvailablePlasmaV2(context.MomentumStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		maxPlasma = dp.FusedAmountToPlasma(amount)
+	} else {
+		availablePlasma, err = vm.AvailablePlasma(context.MomentumStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		maxPlasma = vm.FussedAmountToPlasma(amount)
 	}
 
 	return &PlasmaInfo{
-		CurrentPlasma: available,
-		MaxPlasma:     vm.FussedAmountToPlasma(amount),
+		CurrentPlasma: availablePlasma,
+		MaxPlasma:     maxPlasma,
 		QsrAmount:     amount,
 	}, nil
 }
@@ -231,6 +244,9 @@ type GetRequiredResult struct {
 
 func (a *PlasmaApi) GetRequiredPoWForAccountBlock(param GetRequiredParam) (*GetRequiredResult, error) {
 	_, context, err := api.GetFrontierContext(a.chain, param.SelfAddr)
+	if err != nil {
+		return nil, err
+	}
 	frontierMomentum, err := context.GetFrontierMomentum()
 	if err != nil {
 		return nil, err
@@ -250,31 +266,67 @@ func (a *PlasmaApi) GetRequiredPoWForAccountBlock(param GetRequiredParam) (*GetR
 		return nil, errors.New("toAddress is nil")
 	}
 
-	availablePlasma, err := vm.AvailablePlasma(context.MomentumStore(), context)
-	if err != nil {
-		return nil, err
-	}
-
 	basePlasma, err := vm.GetBasePlasmaForAccountBlock(context, block)
 	if err != nil {
 		return nil, err
 	}
 
-	if availablePlasma > basePlasma {
+	var availablePlasma uint64
+	var requiredFusedPlasma uint64
+
+	if context.IsDynamicPlasmaSporkEnforced() {
+		availablePlasma, err = vm.AvailablePlasmaV2(context.MomentumStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		requiredFusedPlasma = basePlasma * frontierMomentum.NextFusionPrice / dp.PriceScaleFactor
+	} else {
+		availablePlasma, err = vm.AvailablePlasma(context.MomentumStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		requiredFusedPlasma = basePlasma
+	}
+
+	if availablePlasma >= requiredFusedPlasma {
 		return &GetRequiredResult{
 			AvailablePlasma:    availablePlasma,
 			BasePlasma:         basePlasma,
 			RequiredDifficulty: 0,
 		}, nil
 	} else {
-		difficulty, err := vm.GetDifficultyForPlasma(basePlasma - availablePlasma)
-		if err != nil {
-			return nil, err
+		var requiredDifficulty uint64
+		if context.IsDynamicPlasmaSporkEnforced() {
+			effectivePlasma := availablePlasma * dp.PriceScaleFactor / frontierMomentum.NextFusionPrice
+			difficulty, err := dp.GetDifficultyForPlasma(basePlasma - effectivePlasma)
+			if err != nil {
+				return nil, err
+			}
+			requiredDifficulty = difficulty * frontierMomentum.NextWorkPrice / dp.PriceScaleFactor
+		} else {
+			requiredDifficulty, err = vm.GetDifficultyForPlasma(basePlasma - availablePlasma)
+			if err != nil {
+				return nil, err
+			}
 		}
 		return &GetRequiredResult{
 			AvailablePlasma:    availablePlasma,
 			BasePlasma:         basePlasma,
-			RequiredDifficulty: difficulty,
+			RequiredDifficulty: requiredDifficulty,
 		}, nil
 	}
+}
+
+func (a *PlasmaApi) GetVariables() (*definition.PlasmaVariables, error) {
+	_, context, err := api.GetFrontierContext(a.chain, types.PlasmaContract)
+	if err != nil {
+		return nil, err
+	}
+
+	variables, err := definition.GetPlasmaVariables(context.Storage())
+	if err != nil {
+		return nil, err
+	}
+
+	return variables, nil
 }

vm/plasma.go
@@ -8,12 +8,14 @@ import (
 	"github.com/zenon-network/go-zenon/chain/nom"
 	"github.com/zenon-network/go-zenon/chain/store"
 	"github.com/zenon-network/go-zenon/common/types"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/verifier"
 	"github.com/zenon-network/go-zenon/vm/constants"
 	"github.com/zenon-network/go-zenon/vm/embedded"
 	"github.com/zenon-network/go-zenon/vm/vm_context"
 )
 
+// Legacy function from before the Dynamic Plasma spork
 func GetDifficultyForPlasma(requiredPlasma uint64) (uint64, error) {
 	if requiredPlasma > constants.MaxPoWPlasmaForAccountBlock {
 		return 0, constants.ErrForbiddenParam
@@ -22,6 +24,8 @@ func GetDifficultyForPlasma(requiredPlasma uint64) (uint64, error) {
 	}
 	return requiredPlasma * constants.PoWDifficultyPerPlasma, nil
 }
+
+// Legacy function from before the Dynamic Plasma spork
 func DifficultyToPlasma(difficulty uint64) uint64 {
 	// Check for 0
 	if difficulty == 0 {
@@ -36,6 +40,7 @@ func DifficultyToPlasma(difficulty uint64) uint64 {
 	return difficulty / constants.PoWDifficultyPerPlasma
 }
 
+// Legacy function from before the Dynamic Plasma spork
 func FussedAmountToPlasma(amount *big.Int) uint64 {
 	// Check for 0
 	if amount == nil || amount.Sign() <= 0 {
@@ -77,6 +82,35 @@ func AvailablePlasma(momentum store.Momentum, account store.Account) (uint64, er
 	}
 	if answer.Cmp(constants.MaxFussedAmountForAccountBig) == +1 {
 		return constants.MaxFussedAmountForAccount, nil
+	} else {
+		return answer.Uint64(), nil
+	}
+}
+
+// V2 was introduced with the Dynamic Plasma spork
+func AvailablePlasmaV2(momentum store.Momentum, account store.Account) (uint64, error) {
+	address := *account.Address()
+	committed, err := momentum.GetAccountStore(address).GetChainPlasma()
+	if err != nil {
+		return 0, err
+	}
+	fused, err := momentum.GetStakeBeneficialAmount(address)
+	if err != nil {
+		return 0, err
+	}
+	uncommitted, err := account.GetChainPlasma()
+	if err != nil {
+		return 0, err
+	}
+	fusedPlasma := new(big.Int).SetUint64(dp.FusedAmountToPlasma(fused))
+
+	answer := new(big.Int).Add(fusedPlasma, committed)
+	answer = answer.Sub(answer, uncommitted)
+	if answer.Sign() == -1 {
+		return 0, errors.Errorf("got negative available plasma")
+	}
+	if answer.Cmp(dp.MaxFusedAmountForAccountBig) == +1 {
+		return dp.MaxFusedAmountForAccount, nil
 	} else {
 		return answer.Uint64(), nil
 	}

vm/vm.go
@@ -10,6 +10,7 @@ import (
 	"github.com/zenon-network/go-zenon/common"
 	"github.com/zenon-network/go-zenon/common/db"
 	"github.com/zenon-network/go-zenon/common/types"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/vm/constants"
 	"github.com/zenon-network/go-zenon/vm/embedded"
 	"github.com/zenon-network/go-zenon/vm/vm_context"
@@ -50,13 +51,24 @@ func enoughPlasma(context vm_context.AccountVmContext, block *nom.AccountBlock) 
 		return nil
 	}
 
-	available, err := AvailablePlasma(context.MomentumStore(), context)
+	var available uint64
+	var err error
+	if context.IsDynamicPlasmaSporkEnforced() {
+		available, err = AvailablePlasmaV2(context.MomentumStore(), context)
+	} else {
+		available, err = AvailablePlasma(context.MomentumStore(), context)
+	}
 	common.DealWithErr(err)
 	if available < block.FusedPlasma {
 		return constants.ErrNotEnoughPlasma
 	}
 
-	powPlasma := DifficultyToPlasma(block.Difficulty)
+	var powPlasma uint64
+	if context.IsDynamicPlasmaSporkEnforced() {
+		powPlasma = dp.DifficultyToPlasma(block.Difficulty)
+	} else {
+		powPlasma = DifficultyToPlasma(block.Difficulty)
+	}
 	block.TotalPlasma = powPlasma + block.FusedPlasma
 	if block.TotalPlasma > constants.MaxPlasmaForAccountBlock {
 		return constants.ErrBlockPlasmaLimitReached

zenon-network/go-zenon

rpc/api/embedded/plasma.go
@@ -13,6 +13,7 @@ import (
 	"github.com/zenon-network/go-zenon/common"
 	"github.com/zenon-network/go-zenon/common/types"
 	"github.com/zenon-network/go-zenon/consensus"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/rpc/api"
 	"github.com/zenon-network/go-zenon/vm"
 	"github.com/zenon-network/go-zenon/vm/embedded/definition"
@@ -180,14 +181,26 @@ func (a *PlasmaApi) Get(address types.Address) (*PlasmaInfo, error) {
 		return nil, err
 	}
 
-	available, err := vm.AvailablePlasma(context.CacheStore(), context)
-	if err != nil {
-		return nil, err
+	var availablePlasma uint64
+	var maxPlasma uint64
+
+	if context.IsDynamicPlasmaSporkEnforced() {
+		availablePlasma, err = vm.AvailablePlasmaV2(context.CacheStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		maxPlasma = dp.FusedAmountToPlasma(amount)
+	} else {
+		availablePlasma, err = vm.AvailablePlasma(context.CacheStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		maxPlasma = vm.FussedAmountToPlasma(amount)
 	}
 
 	return &PlasmaInfo{
-		CurrentPlasma: available,
-		MaxPlasma:     vm.FussedAmountToPlasma(amount),
+		CurrentPlasma: availablePlasma,
+		MaxPlasma:     maxPlasma,
 		QsrAmount:     amount,
 	}, nil
 }
@@ -236,6 +249,9 @@ type GetRequiredResult struct {
 
 func (a *PlasmaApi) GetRequiredPoWForAccountBlock(param GetRequiredParam) (*GetRequiredResult, error) {
 	_, context, err := api.GetFrontierContext(a.chain, param.SelfAddr)
+	if err != nil {
+		return nil, err
+	}
 	frontierMomentum, err := context.GetFrontierMomentum()
 	if err != nil {
 		return nil, err
@@ -255,31 +271,67 @@ func (a *PlasmaApi) GetRequiredPoWForAccountBlock(param GetRequiredParam) (*GetR
 		return nil, errors.New("toAddress is nil")
 	}
 
-	availablePlasma, err := vm.AvailablePlasma(context.CacheStore(), context)
-	if err != nil {
-		return nil, err
-	}
-
 	basePlasma, err := vm.GetBasePlasmaForAccountBlock(context, block)
 	if err != nil {
 		return nil, err
 	}
 
-	if availablePlasma > basePlasma {
+	var availablePlasma uint64
+	var requiredFusedPlasma uint64
+
+	if context.IsDynamicPlasmaSporkEnforced() {
+		availablePlasma, err = vm.AvailablePlasmaV2(context.CacheStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		requiredFusedPlasma = basePlasma * frontierMomentum.NextFusionPrice / dp.PriceScaleFactor
+	} else {
+		availablePlasma, err = vm.AvailablePlasma(context.CacheStore(), context)
+		if err != nil {
+			return nil, err
+		}
+		requiredFusedPlasma = basePlasma
+	}
+
+	if availablePlasma >= requiredFusedPlasma {
 		return &GetRequiredResult{
 			AvailablePlasma:    availablePlasma,
 			BasePlasma:         basePlasma,
 			RequiredDifficulty: 0,
 		}, nil
 	} else {
-		difficulty, err := vm.GetDifficultyForPlasma(basePlasma - availablePlasma)
-		if err != nil {
-			return nil, err
+		var requiredDifficulty uint64
+		if context.IsDynamicPlasmaSporkEnforced() {
+			effectivePlasma := availablePlasma * dp.PriceScaleFactor / frontierMomentum.NextFusionPrice
+			difficulty, err := dp.GetDifficultyForPlasma(basePlasma - effectivePlasma)
+			if err != nil {
+				return nil, err
+			}
+			requiredDifficulty = difficulty * frontierMomentum.NextWorkPrice / dp.PriceScaleFactor
+		} else {
+			requiredDifficulty, err = vm.GetDifficultyForPlasma(basePlasma - availablePlasma)
+			if err != nil {
+				return nil, err
+			}
 		}
 		return &GetRequiredResult{
 			AvailablePlasma:    availablePlasma,
 			BasePlasma:         basePlasma,
-			RequiredDifficulty: difficulty,
+			RequiredDifficulty: requiredDifficulty,
 		}, nil
 	}
+}
+
+func (a *PlasmaApi) GetVariables() (*definition.PlasmaVariables, error) {
+	_, context, err := api.GetFrontierContext(a.chain, types.PlasmaContract)
+	if err != nil {
+		return nil, err
+	}
+
+	variables, err := definition.GetPlasmaVariables(context.Storage())
+	if err != nil {
+		return nil, err
+	}
+
+	return variables, nil
 }

vm/plasma.go
@@ -8,12 +8,14 @@ import (
 	"github.com/zenon-network/go-zenon/chain/nom"
 	"github.com/zenon-network/go-zenon/chain/store"
 	"github.com/zenon-network/go-zenon/common/types"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/verifier"
 	"github.com/zenon-network/go-zenon/vm/constants"
 	"github.com/zenon-network/go-zenon/vm/embedded"
 	"github.com/zenon-network/go-zenon/vm/vm_context"
 )
 
+// Legacy function from before the Dynamic Plasma spork
 func GetDifficultyForPlasma(requiredPlasma uint64) (uint64, error) {
 	if requiredPlasma > constants.MaxPoWPlasmaForAccountBlock {
 		return 0, constants.ErrForbiddenParam
@@ -22,6 +24,8 @@ func GetDifficultyForPlasma(requiredPlasma uint64) (uint64, error) {
 	}
 	return requiredPlasma * constants.PoWDifficultyPerPlasma, nil
 }
+
+// Legacy function from before the Dynamic Plasma spork
 func DifficultyToPlasma(difficulty uint64) uint64 {
 	// Check for 0
 	if difficulty == 0 {
@@ -36,6 +40,7 @@ func DifficultyToPlasma(difficulty uint64) uint64 {
 	return difficulty / constants.PoWDifficultyPerPlasma
 }
 
+// Legacy function from before the Dynamic Plasma spork
 func FussedAmountToPlasma(amount *big.Int) uint64 {
 	// Check for 0
 	if amount == nil || amount.Sign() <= 0 {
@@ -77,6 +82,35 @@ func AvailablePlasma(cache store.Cache, account store.Account) (uint64, error) {
 	}
 	if answer.Cmp(constants.MaxFussedAmountForAccountBig) == +1 {
 		return constants.MaxFussedAmountForAccount, nil
+	} else {
+		return answer.Uint64(), nil
+	}
+}
+
+// V2 was introduced with the Dynamic Plasma spork
+func AvailablePlasmaV2(cache store.Cache, account store.Account) (uint64, error) {
+	address := *account.Address()
+	committed, err := cache.GetChainPlasma(address)
+	if err != nil {
+		return 0, err
+	}
+	fused, err := cache.GetStakeBeneficialAmount(address)
+	if err != nil {
+		return 0, err
+	}
+	uncommitted, err := account.GetChainPlasma()
+	if err != nil {
+		return 0, err
+	}
+	fusedPlasma := new(big.Int).SetUint64(dp.FusedAmountToPlasma(fused))
+
+	answer := new(big.Int).Add(fusedPlasma, committed)
+	answer = answer.Sub(answer, uncommitted)
+	if answer.Sign() == -1 {
+		return 0, errors.Errorf("got negative available plasma")
+	}
+	if answer.Cmp(dp.MaxFusedAmountForAccountBig) == +1 {
+		return dp.MaxFusedAmountForAccount, nil
 	} else {
 		return answer.Uint64(), nil
 	}

vm/vm.go
@@ -11,6 +11,7 @@ import (
 	"github.com/zenon-network/go-zenon/common"
 	"github.com/zenon-network/go-zenon/common/db"
 	"github.com/zenon-network/go-zenon/common/types"
+	"github.com/zenon-network/go-zenon/dp"
 	"github.com/zenon-network/go-zenon/vm/constants"
 	"github.com/zenon-network/go-zenon/vm/embedded"
 	"github.com/zenon-network/go-zenon/vm/vm_context"
@@ -56,16 +57,25 @@ func enoughPlasma(context vm_context.AccountVmContext, block *nom.AccountBlock) 
 	// Prevent potentially expensive database read operations by only
 	// checking available plasma for blocks with fused plasma
 	if block.FusedPlasma > 0 {
-		available, err := AvailablePlasma(context.CacheStore(), context)
-		if err != nil {
-			return err
+		var available uint64
+		var err error
+		if context.IsDynamicPlasmaSporkEnforced() {
+			available, err = AvailablePlasmaV2(context.CacheStore(), context)
+		} else {
+			available, err = AvailablePlasma(context.CacheStore(), context)
 		}
+		common.DealWithErr(err)
 		if available < block.FusedPlasma {
 			return constants.ErrNotEnoughPlasma
 		}
 	}
 
-	powPlasma := DifficultyToPlasma(block.Difficulty)
+	var powPlasma uint64
+	if context.IsDynamicPlasmaSporkEnforced() {
+		powPlasma = dp.DifficultyToPlasma(block.Difficulty)
+	} else {
+		powPlasma = DifficultyToPlasma(block.Difficulty)
+	}
 	block.TotalPlasma = powPlasma + block.FusedPlasma
 	if block.TotalPlasma > constants.MaxPlasmaForAccountBlock {
 		return constants.ErrBlockPlasmaLimitReached